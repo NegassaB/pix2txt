b4bfecfdf126e5ff0474e01396defa81e438ca82
Negassa Berhanu <negassab16@gmail.com>
2021-12-23 20:17:25 +0300

    receiving & downloading pic file complete, analysis and return of result left

--- src/botter.py
+++ src/botter.py
@@ -1,3 +1,4 @@
+import asyncio
 import enum
 import logging
 import time
@@ -46,6 +47,7 @@ botter = TelegramClient('../pix2txt', API_ID, API_HASH).start(bot_token=TOKEN)
 commands_dict = {
     'start_cmd': "/start",
     'help_cmd': "/help",
+    'convert_pic': "/convert_pic",
 }
 
 operation_status = dict()
@@ -54,27 +56,52 @@ operation_status = dict()
 class States(enum.Enum):
     START = enum.auto()
     RECV_PIC = enum.auto()
+    AWAIT_RESULT = enum.auto()
 
 # todo: get pic from user
 # todo: handle forwarded pix as well
 # todo: store the image with some sort of id on the server
-# todo: give image to ocrspace https://github.com/ErikBoesen/ocrspace/blob/master/example.py
 # todo: return result as text & post to the group
 
 
 @botter.on(events.NewMessage)
 async def msg_event_handler(event):
-    # todo: on start get user's telegram id
-    # todo: insert into db perhaps
     user_id = event.sender_id
-    if event.raw_text.lower() == "/start":
+    if event.raw_text.lower() == commands_dict['start_cmd']:
         await event.respond(
-            f"hello {user_id} and welcome to pix2txt, please send the picture you wish to convert to text"
+            f"hello {user_id} and welcome to pix2txt, please send /convert_pic"
         )
-        operation_status[user_id]['state'] = States.START
-    elif event.photo:
-        await event.reply("downloading and analyzing picture")
-        operation_status[user_id]['state'] = States.RECV_PIC
+        operation_status[user_id] = {'state': States.START}
+    elif event.raw_text.lower() == commands_dict['help_cmd']:
+        # todo: insert helpful message here
+        pass
+    elif user_id in operation_status.keys():
+        current_state = operation_status[user_id]['state']
+        if event.raw_text.lower() == commands_dict['convert_pic']:
+            await event.reply("Please send the picture you wish to convert to text")
+            operation_status[user_id]['state'] = States.RECV_PIC
+        elif current_state == States.RECV_PIC:
+            if event.photo:
+                # hack: has id attribute for the pic use it to distinguish pic files
+                # hack: download pic file, give to ocr lib return result
+                await event.reply("downloading and analyzing picture, please WAIT")
+                operation_status[user_id]['state'] = States.AWAIT_RESULT
+                pic_file = await botter.download_media(event.photo, file="pix/")
+                if not pic_file:
+                    await event.reply("Please send a picture file, nothing else")
+                    await event.delete()
+                    operation_status[user_id]['state'] == States.RECV_PIC
+                else:
+                    # hack: give image to ocrspace https://github.com/ErikBoesen/ocrspace/blob/master/example.py
+                    print(type(pic_file))
+                    # with open(f'pix/{event.photo.id}', 'w') as pic_writer:
+                    #     pic_writer.write(pic_file)
+            else:
+                await event.reply("Please send a picture file, nothing else")
+                await event.delete()
+                operation_status[user_id]['state'] == States.RECV_PIC
+        # elif current_state == States.AWAIT_RESULT:
+        #     pass
     else:
         await event.reply("You have to /start the bot")
 
